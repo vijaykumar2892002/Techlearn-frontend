eb9a8c3227b2009a4715f9b623c2631db692162b
Vijay <vkumar80090@gmail.com>
2024-07-29 06:12:33 +0530

    cors-config

--- server/index.js
+++ server/index.js
@@ -1,60 +1,68 @@
 const express = require("express");
-const app=express();
-
-//import all Routes
-const userRoutes=require("./Routers/User")
-const courseRoutes=require("./Routers/Course")
-const paymentRoutes=require("./Routers/Payment")
-const profileRoutes=require("./Routers/Profile")
-const contactUsRoute = require("./Routers/Contact");
-const database = require("./Config/database");
+const app = express();
+const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const {cloudinaryConnect } = require("./Config/cloudinary");
 const fileUpload = require("express-fileupload");
-const dotenv = require("dotenv");
 
+// Load environment variables
 dotenv.config();
 const PORT = process.env.PORT || 4000;
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
 
-//database connect
+// Import all Routes
+const userRoutes = require("./Routers/User");
+const courseRoutes = require("./Routers/Course");
+const paymentRoutes = require("./Routers/Payment");
+const profileRoutes = require("./Routers/Profile");
+const contactUsRoute = require("./Routers/Contact");
+const database = require("./Config/database");
+const { cloudinaryConnect } = require("./Config/cloudinary");
+
+// Connect to database
 database();
-//middlewares
+
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
-)
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || [CLIENT_ORIGIN, "http://localhost:3000"].indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(
-	fileUpload({
-		useTempFiles:true,
-		tempFileDir:"/tmp",
-	})
-)
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
 
-//cloudinary connection
+// Cloudinary connection
 cloudinaryConnect();
 
-
-//routes
+// Routes
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/reach", contactUsRoute);
 
-//def route
-
+// Default route
 app.get("/", (req, res) => {
-	return res.json({
-		success:true,
-		message:'Your server is up and running....'
-	});
+  return res.json({
+    success: true,
+    message: 'Your server is up and running....'
+  });
 });
 
+// Start the server
 app.listen(PORT, () => {
-	console.log(`App is running at ${PORT}`)
-})
+  console.log(`App is running at ${PORT}`);
+});
